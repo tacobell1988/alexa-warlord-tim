4da29d527d5a0d2c6e810cc7ebb7054f56968a9e
tacobell1988 <34141697+tacobell1988@users.noreply.github.com>
2017-11-30 14:55:56 -0600

    Update tickets.js

--- lambda/custom/tickets.js
+++ lambda/custom/tickets.js
@@ -1,1 +1,4 @@
-const responses = [`The status of ticket ${ticketNumber} is open`];
+const statusResponses = [`The status of ticket ${ticketNumber} is open`,`Ticket ${ticketNumber} has been vanquished`];
+const countResponses = [`Sir you have Eight open tickets`,`Eight tickets are awaiting your attention`, `The lowly squire has informed me that you have Eight tickets`];
+const assignResponses = [`I have assigned ticket ${ticketNumber} to ${employee}`,`${employee} has been charged with completing ${ticketNumber} has been vanquished`];
+const closeResponses = [`I have closed ticket ${ticketNumber}`, `Ticket ${ticketNumber} has been elimanated`];
